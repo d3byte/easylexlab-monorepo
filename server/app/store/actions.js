d008adcdf8b2e2534b44584a481f7b2ceefb31b0
Pandinator <cheloveckov.ru@yandex.ru>
2017-06-17 11:55:59 +0300

    A scalable storage

--- server/app/store/actions.js
+++ server/app/store/actions.js
@@ -1,7 +1,3 @@
 export default {
-  register({ commit }, userId) {
-    setTimeout(() => {
-      commit('register', userId);
-    }, 1000);
-  }
+  
 }
--- server/app/store/getters.js
+++ server/app/store/getters.js
@@ -1,13 +1,3 @@
 export default {
-  unregisteredUsers(state) {
-      return state.users.filter(user => {
-          return !user.registered;
-      });
-  },
-  registrations(state) {
-      return state.registrations;
-  },
-  totalRegistrations(state) {
-      return state.registrations.length;
-  }
+  
 }
