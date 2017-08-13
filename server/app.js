814d8c9e2fc629042d7dc4b8e670a1da6b832893
Pandinator <cheloveckov.ru@yandex.ru>
2017-08-13 20:54:29 +0300

    Production build

--- server/app.js
+++ server/app.js
@@ -20,44 +20,6 @@ const app = express();
 // Public folder
 const folder = __dirname + "/app/";
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(folder + "index.html"));
-});
-app.get('/signup', (req, res) => {
-    res.sendFile(path.join(folder + "index.html"));
-});
-app.get('/profile', (req, res) => {
-    res.sendFile(path.join(folder + "index.html"));
-});
-app.get('/group/:id', (req, res) => {
-    res.sendFile(path.join(folder + "index.html"));
-});
-app.get('/group/:id/newtask', (req, res) => {
-    res.sendFile(path.join(folder + "index.html"));
-});
-app.get('/profile/settings', (req, res) => {
-    res.sendFile(path.join(folder + "index.html"));
-});
-app.get('/profile/newgroup', (req, res) => {
-    res.sendFile(path.join(folder + "index.html"));
-});
-app.get('/logout', (req, res) => {
-    res.sendFile(path.join(folder + "index.html"));
-});
-app.get('/eula', (req, res) => {
-    res.sendFile(path.join(folder + "index.html"));
-});
-app.get('/task/:id', (req, res) => {
-    res.sendFile(path.join(folder + "index.html"));
-});
-
-app.use('/', express.static(folder));
-app.use('/profile', express.static(folder));
-app.use('/group', express.static(folder));
-app.use('/group/:id', express.static(folder));
-app.use('/task', express.static(folder));
-app.use('/task/:id', express.static(folder));
-
 // Middleware
 app.use(bodyParser.json());
 app.use('/api', routes);
