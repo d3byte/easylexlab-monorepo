b28220e635ef059afeb6f6ffb2fd913f16b40b49
Pandinator <cheloveckov.ru@yandex.ru>
2017-08-13 21:17:28 +0300

    Aa

---
+++ server/app/components/Profile.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <app-header style="margin-bottom: 80px;"/>
+    <admin v-if="user.permissions == 'admin'" />
+    <teacher v-if="this.user.permissions == 'teacher'" />
+    <student v-if="user.permissions == 'student'" />
+  </div>
+</template>
+
+<script>
+import Header from './Header.vue';
+import Teacher from './Teacher.vue';
+import Student from './Student.vue';
+import Admin from './Admin.vue';
+
+export default {
+
+  data() {
+    return {
+      isAdmin: '',
+      isTeacher: '',
+      isStudent: ''
+    }
+  },
+  computed: {
+    user() {
+      return this.$store.getters.user
+    },
+  },
+  components: {
+    'app-header': Header,
+    'admin': Admin,
+    'teacher': Teacher,
+    'student': Student
+  },
+  created() {
+    if(!this.$store.getters.loginState)
+      this.$router.push('/login');
+    this.isAdmin = (this.user.permissions == 'admin' ? true : false);
+    this.isTeacher = (this.user.permissions == 'teacher' ? true : false);
+    this.isStudent = (this.user.permissions == 'student' ? true : false);
+  }
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ server/app/components/Scramble.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+  <div>
+    <button class="btn back" @click="hideGames">Назад</button>
+</div>
+</template>
+
+<script>
+import _ from 'lodash';
+export default {
+  props: ['stack'],
+  data() {
+    return {
+      currentPair: {},
+      pairs: []
+      // done: false
+    }
+  },
+  computed: {
+    showTest() {
+      return this.$store.getters.testAvailable
+    }
+  },
+  http: {
+    root: '/api'
+  },
+  methods: {
+    hideGames() {
+      this.$store.dispatch('hideGames');
+    }
+  },
+  components: {
+    draggable,
+  }
+}
+</script>
+
+<style lang="css">
+</style>
