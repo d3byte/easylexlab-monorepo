6b2a3dc34c0455e9a153cc33ddfe630d8b0f345d
we2beast <89505105005@mail.ru>
2017-08-17 17:05:19 +0300

    Server change

--- server/controllers/stackController.js
+++ server/controllers/stackController.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken';
+import lodash from 'lodash';
 import secret from './../secret';
 
 import db from './../models';
@@ -9,92 +10,98 @@ const stackController = {};
 
 // New stack
 stackController.post = (req, res) => {
-  const {
-    name,
-    tasks,
-    timeToDo,
-    groupId,
-    attempts
-  } = req.body;
-
-  // TODO: use lodash to refactor the code
-
-  const user = req.user;
-
-  if(user.permissions == "teacher" || user.permissions == "admin") {
-      const stack = new db.Stack({
+    const {
         name,
         tasks,
         timeToDo,
-        _group: groupId,
+        groupId,
         attempts
-      });
-      console.log(stack);
-      stack.save().then(stack => {
-        res.status(200).json({
-          success: true,
-          stack
+    } = req.body;
+
+    // TODO: use lodash to refactor the code
+
+    const user = req.user;
+
+    if (user.permissions == "teacher" || user.permissions == "admin") {
+        const stack = new db.Stack({
+            name,
+            tasks,
+            timeToDo,
+            _group: groupId,
+            attempts
         });
-      }).catch(err => {
-        res.status(500).json({err: 'lel'});
-      });
+        if (check.isEmpty(stack)) {
+            stack.save().then(stack => {
+                res.status(200).json({
+                    success: true,
+                    stack
+                });
+            }).catch(err => {
+                res.status(500).json({err: 'lel'});
+            });
+        } else {
+            res.status(500).json({err: 'lel'});
+        }
     }
 };
 
 // Get stacks of one exact group
 stackController.getTests = (req, res) => {
-  const groupId = req.body.groupId;
-  const user = req.user;
-  if(user.permissions == 'teacher' || user.permissions == 'admin' || user.permissions == 'student') {
-    db.Stack.find({_group: groupId}).populate({ path: '', select: 'name _group tasks results timeToDo' }).then(stacks => {
-      res.status(200).json({ stacks });
-    }).catch(err => {
-      res.status(500).json({ err });
-    });
-  } else res.status(501).json({'error':'No permissions for this action'});
+    const groupId = req.body.groupId;
+    const user = req.user;
+    if (user.permissions == 'teacher' || user.permissions == 'admin' || user.permissions == 'student') {
+        db.Stack.find({_group: groupId}).populate({
+            path: '',
+            select: 'name _group tasks results timeToDo'
+        }).then(stacks => {
+            res.status(200).json({stacks});
+        }).catch(err => {
+            res.status(500).json({err});
+        });
+    } else res.status(501).json({'error': 'No permissions for this action'});
 };
 
 // Get one exact stack
 stackController.getTest = (req, res) => {
-  const testId = req.body.testId;
-  const user = req.user;
-  if(user.permissions == 'teacher' || user.permissions == 'admin' || user.permissions == 'student') {
-    db.Stack.findById(testId).populate({ path: '', select: 'name tasks results test timeToDo' }).then(stack => {
-      res.status(200).json({ stack });
-    }).catch(err => {
-      res.status(500).json({ err });
-    });
-  } else res.status(501).json({'error':'No permissions for this action'});
+    const testId = req.body.testId;
+    const user = req.user;
+    if (user.permissions == 'teacher' || user.permissions == 'admin' || user.permissions == 'student') {
+        db.Stack.findById(testId).populate({path: '', select: 'name tasks results test timeToDo'}).then(stack => {
+            res.status(200).json({stack});
+        }).catch(err => {
+            res.status(500).json({err});
+        });
+    } else res.status(501).json({'error': 'No permissions for this action'});
 };
 
 // Add result to the stack
 stackController.addResult = (req, res) => {
-  const {
-    result,
-    stackId,
-    name,
-    username,
-    userId
-  } = req.body;
+    const {
+        result,
+        stackId,
+        name,
+        username,
+        userId
+    } = req.body;
 
-  const user = req.user;
+    const user = req.user;
 
-  const results = {
-    userId: user.id,
-    username,
-    name,
-    result
-  };
+    const results = {
+        userId: user.id,
+        username,
+        name,
+        result
+    };
 
-  db.Stack.findById(stackId).then(stack => {
-    stack.results.push(results);
-    stack.save();
-    res.json({
-      success: true
+    db.Stack.findById(stackId).then(stack => {
+        stack.results.push(results);
+        stack.save();
+        res.json({
+            success: true
+        });
+    }).catch(err => {
+        throw err;
     });
-  }).catch(err => {
-    throw err;
-  });
 };
 
 export default stackController;
