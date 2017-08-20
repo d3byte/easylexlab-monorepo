35a6a46fc118b00f82f6184986a06795265d9b1a
we2beast <89505105005@mail.ru>
2017-08-20 21:11:09 +0300

    Sign Up Already, Header 50/50

--- server/app/components/Home.vue
+++ server/app/components/Home.vue
@@ -1,21 +1,25 @@
 <template>
-  <div>
-    <app-header></app-header>
-    <h1>Landing page</h1>
-    <!-- <mail></mail> -->
-  </div>
+    <div>
+        <app-header></app-header>
+        <h1>Landing page</h1>
+        <login></login>
+        <!-- <mail></mail> -->
+    </div>
 </template>
 
 <script>
-import Header from './Header.vue';
-import SendMail from './SendMail.vue';
 
-export default {
-  components: {
-    'app-header': Header,
-    'mail': SendMail
-  }
-}
+    import Header from './Header.vue';
+    import SendMail from './SendMail.vue';
+    import Login from './Login.vue';
+
+    export default {
+        components: {
+            'app-header': Header,
+            'mail': SendMail,
+            'login': Login
+        }
+    }
 </script>
 
 <style lang="css">
