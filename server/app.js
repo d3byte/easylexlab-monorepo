51422a1d5b95a9d9c6ca6ae9133997a7ae170085
Pandinator <cheloveckov.ru@yandex.ru>
2017-08-27 02:00:34 +0300

    again added cors

--- server/app.js
+++ server/app.js
@@ -4,6 +4,7 @@ import bodyParser from 'body-parser';
 import expressJWT from 'express-jwt';
 import jwt from 'jsonwebtoken';
 import path from 'path';
+import cors from 'cors';
 
 import secret from './secret';
 
@@ -65,6 +66,7 @@ app.use('/task', express.static(folder));
 app.use('/task/:id', express.static(folder));
 
 // Middleware
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', routes);
 // app.use(expressJWT({ secret: secret }).unless({ path: ['api/login', '/'] }));
