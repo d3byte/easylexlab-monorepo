9cba9a0e4507a5d3ef819d029959cbfcf09bf25d
DieRK <russiankenny@gmail.com>
2017-08-26 23:08:23 +0300

    Just a backup, please don't watch this.

--- server/app/components/Stack.vue
+++ server/app/components/Stack.vue
@@ -1,38 +1,56 @@
 <template>
-  <div>
-    <app-header style="margin-bottom: 60px;"></app-header>
-    <flashcards v-if="games.flashcards.show" :stack="task"></flashcards>
-    <matching v-if="games.matching.show" :stack="task"></matching>
-    <snake v-if="games.snake.show" :stack="task"></snake>
-    <scramble v-if="games.scramble.show" :stack="task"></scramble>
-    <test v-if="showTest" :stack="task"></test>
-    <div v-show="!showTest && !games.matching.show && !games.flashcards.show && !games.snake.show && !games.scramble.show">
-      <div class="container">
-        <div class="row box">
-          <center>
-            <h2>{{ task.name }}</h2>
-              <div class="text game">Выбрать игру</div>
-              <div class="menu">
-                <div @click="showMatching" class="item">
-                  Matching
-                </div>
-                <div @click="showFlashcards" class="item">
-                  Flashcards
-                </div>
-                <div @click="showSnake" class="item">
-                  Snake
-                </div>
-                <div @click="showScramble" class="item">
-                  Word Scramble
-                </div>
-              </div>
-            <button v-if="testAvailable" @click="tryTest" class="btn">Пройти тест</button>
-          </center>
+<div>
+  <app-header></app-header>
+
+  <div class="item">
+    <div class="item-bg">
+      <img src="../assets/images/a6.jpg" class="blur opacity-3">
+    </div>
+
+    <div class="p-a-md">
+      <div class="row m-t">
+        <div class="col-sm-7">
+          <div class="clear m-b">
+            <h3 class="m-a-0 m-b-xs">{{ task.name }}</h3>
+            <h4> Выберите игру: </h4>
+          </div>
+        </div>
+        <div class="col-sm-5">
         </div>
       </div>
+    </div>
+  </div>
+
+
+  <div class="padding" id="gameblock" v-show="!showTest && !games.matching.show && !games.flashcards.show && !games.snake.show && !games.scramble.show">
+    <div class="row">
+    <div class="col-sm-6">
+      <div class="col box">
+        <div class="row-col success white-text text-center">
+          <h1>Учить</h1></div>
+        <div class="p-a text-center">
+          <h4 @click="showMatching">Matching</h4>
+          <h4 @click="showFlashcards">Flashcards</h4>
+        </div>
+      </div>
+    </div>
+
+    <div class="col-sm-6">
+      <div class="col box">
+        <div class="row-col warn white-text text-center">
+          <h1>Играть</h1></div>
+        <div class="p-a text-center">
+          <h4 @click="showSnake">Змейка</h4>
+          <h4 @click="showScramble">Word Scramble</h4>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
+            <!-- <button v-if="testAvailable" @click="tryTest" class="btn">Пройти тест</button> -->
       <div class="container">
         <div class="row box">
-          <center>
             <table class="table table-striped table-hover">
               <thead>
               <tr>
@@ -47,11 +65,29 @@
               </tr>
             </tbody>
             </table>
-          </center>
         </div>
       </div>
+
+      <div class="col-sm-3"></div>
+      <div class="col-sm-3" v-if="testAvailable">
+        <div class="col box">
+          <div class="row-col danger white-text text-center">
+            <h1>Тест</h1></div>
+          <div class="p-a text-center">
+            <h4 @click="tryTest">Пройти тест</h4>
+          </div>
+        </div>
+      </div>
+      <div class="col-sm-3"></div>
+
+      <flashcards v-if="games.flashcards.show" :stack="task"></flashcards>
+      <matching v-if="games.matching.show" :stack="task"></matching>
+      <snake v-if="games.snake.show" :stack="task"></snake>
+      <scramble v-if="games.scramble.show" :stack="task"></scramble>
+      <test v-if="showTest" :stack="task"></test>
+
     </div>
-  </div>
+
 </template>
 
 <script>
@@ -90,28 +126,30 @@ export default {
     root: '/api'
   },
   created() {
-    if(!this.$store.getters.loginState || this.user.permissions != 'student') {
+    if (!this.$store.getters.loginState || this.user.permissions != 'student') {
       this.$router.push('/profile');
     }
-    this.$http.post('gettest', { testId: this.testId }, {
+    this.$http.post('gettest', {
+      testId: this.testId
+    }, {
       headers: {
-        'Content-type' : 'application/json',
+        'Content-type': 'application/json',
         'Authorization': 'Bearer ' + this.$store.getters.userToken
       }
     }).then(res => {
       this.task = res.body.stack;
       var haveThisStack = false;
-      for(let group of this.user._groups) {
-        if(haveThisStack)
+      for (let group of this.user._groups) {
+        if (haveThisStack)
           break;
-        for(let stack of group._tests) {
-          if(stack._id == this.task._id) {
+        for (let stack of group._tests) {
+          if (stack._id == this.task._id) {
             haveThisStack = true;
             break;
           }
         }
       }
-      if(!haveThisStack)
+      if (!haveThisStack)
         this.$router.push('/profile');
       this.$store.dispatch('setGames', this.task.attempts);
     });
@@ -160,4 +198,22 @@ export default {
     color: black;
     text-shadow: none;
   }
+
+  .white-text {
+    color: white;
+    padding: 35px;
+  }
+
+  #gameblock {
+    margin-top: 20px;
+  }
+
+  .col-container {
+    display: table;
+    width: 100%;
+}
+
+  .col {
+    display: table-cell;
+}
 </style>
