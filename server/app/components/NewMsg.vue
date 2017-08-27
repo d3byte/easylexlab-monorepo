9b9acc00a5dcdbc81aa23e8f4cb71a73edf08706
Pandinator <cheloveckov.ru@yandex.ru>
2017-08-27 13:05:20 +0300

    Renamed seng msg button

--- server/app/components/NewMsg.vue
+++ server/app/components/NewMsg.vue
@@ -18,7 +18,7 @@
                 <textarea v-model="text" placeholder="Сообщение" required></textarea>
               </div> -->
               <center>
-                <button @click="send" class="btn dark-white p-x-md" v-if="!success">Создать</button>
+                <button @click="send" class="btn dark-white p-x-md" v-if="!success">Отправить</button>
                 <button @click="refresh" type="button" class="btn dark-white p-x-md" data-dismiss="modal" v-if="!success">Отмена</button>
                 <button @click="refresh" type="button" class="btn dark-white p-x-md" data-dismiss="modal" v-else>Закрыть</button>
               </center>
