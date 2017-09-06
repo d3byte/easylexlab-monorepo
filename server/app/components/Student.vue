49b884c2c23913121b226ef854785f5f2a60d374
Pandinator <cheloveckov.ru@yandex.ru>
2017-09-06 23:19:06 +0300

    show proper msg if there is no closest deadline

--- server/app/components/Student.vue
+++ server/app/components/Student.vue
@@ -26,7 +26,8 @@
             <p class="text-muted"><span class="m-r">{{ token.permissions == 'student' ? 'Ученик' : 'Учитель' }}</span> <small><i class="fa fa-map-marker m-r-xs"></i>{{ school }}, {{ city }}</small></p>
             <h5 class="m-a-0 text-md text-muted">Слов выучено: <b>{{ wordsLearnt }}</b></h5>
             <br>
-            <h5 class="m-a-0 text-md text-muted">Ближайший день сдачи: <b> {{ date }}</b></h5>
+            <h5 class="m-a-0 text-md text-muted" v-if="date">Ближайший день сдачи: <b> {{ date }}</b></h5>
+            <h5 class="m-a-0 text-md text-muted" v-else>Ближайшего дня сдачи нет</h5>
           </div>
         </div>
         <div class="col-md-4 ad hidden-sm-down">
