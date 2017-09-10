3cee503fb436eb27aaa17c87bb00927a3c069ef7
Pandinator <cheloveckov.ru@yandex.ru>
2017-09-10 18:11:54 +0300

    pls

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import app from './app';
 import routes from './routes';
 
-const port = process.env.PORT || 7990;
+const port = 7990;
 
 app.listen(port, () => {
   console.log(`Running on port ${port}`);
