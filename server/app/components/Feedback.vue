797bce3b36ab49c31bd0c1e00a5fd3fe99562b5d
Pandinator <cheloveckov.ru@yandex.ru>
2017-10-04 03:09:08 +0300

    redesigned feedback modal

--- server/app/components/Feedback.vue
+++ server/app/components/Feedback.vue
@@ -2,11 +2,16 @@
     <div id="feedback" class="modal fade" data-backdrop="true" style="display: none;" aria-hidden="true">
       <div class="modal-dialog">
         <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title">Новое сообщение</h5>
+          <div class="modal-header" style="background:rgb(161, 196, 226);">
+            <h5 class="modal-title text-center text-white">
+              <b>
+                <i class="material-icons">&#xE0C9;</i>
+                ОСТАВЬТЕ ОТЗЫВ
+              </b>
+            </h5>
           </div>
           <div class="modal-body text-center p-lg">
-            <h5 v-if="success" class="success">Сообщение успешно отправлено.</h5>
+            <h5 v-if="success">Сообщение успешно отправлено.</h5>
             <form class="login-form" onsubmit="return false">
               <div class="form-group row">
                 <label class="col-sm-4 form-control-label">ФИО</label>
@@ -20,16 +25,16 @@
                   <input type="email" v-model="email" required>
                 </div>
               </div>
-              <div class="form-group row">
+              <div class="form-group row" style="margin-bottom:20px;">
                 <label class="col-sm-4 form-control-label">Отзыв</label>
                 <div class="col-sm-8">
                   <textarea class="form-control" v-model="text" required></textarea>
                 </div>
               </div>
               <center>
-                <button @click="send" class="btn dark-white p-x-md" v-if="!success">Создать</button>
-                <button @click="refresh" type="button" class="btn dark-white p-x-md" data-dismiss="modal" v-if="!success">Отмена</button>
-                <button @click="refresh" type="button" class="btn dark-white p-x-md" data-dismiss="modal" v-else>Закрыть</button>
+                <button @click="send" style="background:rgb(251, 106, 33);color:white;margin-right:10px" class="btn" v-if="!success">Создать</button>
+                <button @click="refresh" style="background:rgb(207, 208, 209);color:white" type="button" class="btn" data-dismiss="modal" v-if="!success">Отмена</button>
+                <button @click="refresh" style="background:rgb(207, 208, 209);color:white" type="button" class="btn" data-dismiss="modal" v-else>Закрыть</button>
               </center>
             </form>
           </div>
