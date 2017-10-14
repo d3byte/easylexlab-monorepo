24708c69ad74d95d54595fe1da9805026b37b220
Pandinator <cheloveckov.ru@yandex.ru>
2017-10-14 14:09:02 +0300

    some fixes

--- server/app/components/Header.vue
+++ server/app/components/Header.vue
@@ -134,7 +134,7 @@
           <li class="nav-item dropdown pos-stc-xs hidden-sm-down">
             <a class="nav-link" href data-toggle="dropdown">
               <i class="material-icons">&#xe7f5;</i>
-              <span v-if="!!notifications.length" class="label label-sm up warn">{{ notifications.length }}</span>
+              <span v-if="user && user.notifications" class="label label-sm up warn">{{ user.notifications.length }}</span>
             </a>
             <!-- dropdown -->
             <div class="dropdown-menu pull-right w-xl animated fadeInUp no-bg no-border no-shadow">
@@ -413,8 +413,8 @@
                 }
               }).then(res => {
                 this.$store.dispatch('userInfo', res.body.user);
-                this.$store.dispatch('changeCurrentGroup', res.body.user._groups[0]);
                 this.notifications = res.body.user.notifications;
+                this.$store.dispatch('changeCurrentGroup', res.body.user._groups[0]);
                 EventBus.$emit('requested-header');
                 });
             },
