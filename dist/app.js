5c14a324ac83d2eaad3e37ca0fede94f699d1d12
Pandinator <cheloveckov.ru@yandex.ru>
2017-10-31 08:52:02 +0300

    server: disabled cors whitelist due to some needs

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 Object.defineProperty(exports, "__esModule", {
-    value: true
+  value: true
 });
 
 var _express = require('express');
@@ -32,6 +32,10 @@ var _cors = require('cors');
 
 var _cors2 = _interopRequireDefault(_cors);
 
+var _quickthumb = require('quickthumb');
+
+var _quickthumb2 = _interopRequireDefault(_quickthumb);
+
 var _secret = require('./secret');
 
 var _secret2 = _interopRequireDefault(_secret);
@@ -43,27 +47,96 @@ var _routes2 = _interopRequireDefault(_routes);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 _mongoose2.default.connect('mongodb://localhost:27017/eng_proj', {
-    useMongoClient: true
+  useMongoClient: true
 }, function () {
-    console.log('Connected to mongodb');
+  console.log('Connected to mongodb');
 });
 
 var app = (0, _express2.default)();
 
+// Public folder
+// const folder = __dirname + "/app/";
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(folder + "index.html"));
+// });
+// app.get('/signup', (req, res) => {
+//     res.sendFile(path.join(folder + "index.html"));
+// });
+// app.get('/signup/student', (req, res) => {
+//     res.sendFile(path.join(folder + "index.html"));
+// });
+// app.get('/signup/teacher', (req, res) => {
+//     res.sendFile(path.join(folder + "index.html"));
+// });
+// app.get('/profile', (req, res) => {
+//     res.sendFile(path.join(folder + "index.html"));
+// });
+// app.get('/profile/msg', (req, res) => {
+//     res.sendFile(path.join(folder + "index.html"));
+// });
+// app.get('/group/:id', (req, res) => {
+//     res.sendFile(path.join(folder + "index.html"));
+// });
+// app.get('/group/:id/newtask', (req, res) => {
+//     res.sendFile(path.join(folder + "index.html"));
+// });
+// app.get('/settings', (req, res) => {
+//     res.sendFile(path.join(folder + "index.html"));
+// });
+// app.get('/profile/newgroup', (req, res) => {
+//     res.sendFile(path.join(folder + "index.html"));
+// });
+// app.get('/stats', (req, res) => {
+//     res.sendFile(path.join(folder + "index.html"));
+// });
+// app.get('/login', (req, res) => {
+//     res.sendFile(path.join(folder + "index.html"));
+// });
+// app.get('/forgotpassword', (req, res) => {
+//     res.sendFile(path.join(folder + "index.html"));
+// });
+// app.get('/recover/:token', (req, res) => {
+//     res.sendFile(path.join(folder + "index.html"));
+// });
+// app.get('/info/rules', (req, res) => {
+//     res.sendFile(path.join(folder + "index.html"));
+// });
+// app.get('/task/:id', (req, res) => {
+//     res.sendFile(path.join(folder + "index.html"));
+// });
+
+// app.use(qt.static(__dirname + '/'));
+// app.use('/', express.static(folder));
+// app.use('/signup', express.static(folder));
+// app.use('/signup/student', express.static(folder));
+// app.use('/signup/teacher', express.static(folder));
+// app.use('/profile', express.static(folder));
+// app.use('/group', express.static(folder));
+// app.use('/group/:id', express.static(folder));
+// app.use('/task', express.static(folder));
+// app.use('/task/:id', express.static(folder));
+// app.use('/recover', express.static(folder));
+// app.use('/recover/:token', express.static(folder));
+// app.use('/info', express.static(folder));
+// app.use('/info/rules', express.static(folder));
+
 // Middleware
-var whitelist = ['http://cf82612.tmweb.ru', 'http://easylexlab.ru'];
-var corsOptions = {
-    origin: function origin(_origin, callback) {
-        if (whitelist.indexOf(_origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-};
-app.use((0, _cors2.default)(corsOptions));
-app.use(_bodyParser2.default.json());
+// var whitelist = ['http://cf82612.tmweb.ru', 'http://easylexlab.ru'];
+// var corsOptions = {
+//     origin: function (origin, callback) {
+//         if (whitelist.indexOf(origin) !== -1) {
+//             callback(null, true)
+//         } else {
+//             callback(new Error('Not allowed by CORS'))
+//         }
+//     }
+// }
+// app.use(cors());
+// app.use(cors(corsOptions));
+app.use((0, _cors2.default)());
 app.use('/api', _routes2.default);
+// app.use(expressJWT({ secret: secret }).unless({ path: ['api/login', '/'] }));
 
 exports.default = app;
 //# sourceMappingURL=app.js.map
