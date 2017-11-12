a293a7255b7d72ff80a21ad843bc393585a9467b
Pandinator <cheloveckov.ru@yandex.ru>
2017-11-12 14:29:36 +0300

    server build

--- dist/app.js
+++ dist/app.js
@@ -32,6 +32,10 @@ var _cors = require('cors');
 
 var _cors2 = _interopRequireDefault(_cors);
 
+var _quickthumb = require('quickthumb');
+
+var _quickthumb2 = _interopRequireDefault(_quickthumb);
+
 var _secret = require('./secret');
 
 var _secret2 = _interopRequireDefault(_secret);
@@ -50,20 +54,76 @@ _mongoose2.default.connect('mongodb://localhost:27017/eng_proj', {
 
 var app = (0, _express2.default)();
 
-// Middleware
-var whitelist = ['http://cf82612.tmweb.ru', 'http://easylexlab.ru'];
-var corsOptions = {
-    origin: function origin(_origin, callback) {
-        if (whitelist.indexOf(_origin) !== -1) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
-    }
-};
-app.use((0, _cors2.default)(corsOptions));
+// Public folder
+var folder = __dirname + "/app/";
+
+app.get('/', function (req, res) {
+    res.sendFile(_path2.default.join(folder + "index.html"));
+});
+app.get('/signup', function (req, res) {
+    res.sendFile(_path2.default.join(folder + "index.html"));
+});
+app.get('/signup/student', function (req, res) {
+    res.sendFile(_path2.default.join(folder + "index.html"));
+});
+app.get('/signup/teacher', function (req, res) {
+    res.sendFile(_path2.default.join(folder + "index.html"));
+});
+app.get('/profile', function (req, res) {
+    res.sendFile(_path2.default.join(folder + "index.html"));
+});
+app.get('/profile/msg', function (req, res) {
+    res.sendFile(_path2.default.join(folder + "index.html"));
+});
+app.get('/group/:id', function (req, res) {
+    res.sendFile(_path2.default.join(folder + "index.html"));
+});
+app.get('/group/:id/newtask', function (req, res) {
+    res.sendFile(_path2.default.join(folder + "index.html"));
+});
+app.get('/settings', function (req, res) {
+    res.sendFile(_path2.default.join(folder + "index.html"));
+});
+app.get('/profile/newgroup', function (req, res) {
+    res.sendFile(_path2.default.join(folder + "index.html"));
+});
+app.get('/stats', function (req, res) {
+    res.sendFile(_path2.default.join(folder + "index.html"));
+});
+app.get('/login', function (req, res) {
+    res.sendFile(_path2.default.join(folder + "index.html"));
+});
+app.get('/forgotpassword', function (req, res) {
+    res.sendFile(_path2.default.join(folder + "index.html"));
+});
+app.get('/recover/:token', function (req, res) {
+    res.sendFile(_path2.default.join(folder + "index.html"));
+});
+app.get('/info/rules', function (req, res) {
+    res.sendFile(_path2.default.join(folder + "index.html"));
+});
+app.get('/task/:id', function (req, res) {
+    res.sendFile(_path2.default.join(folder + "index.html"));
+});
+
+app.use(_quickthumb2.default.static(__dirname + '/'));
+app.use('/', _express2.default.static(folder));
+app.use('/signup', _express2.default.static(folder));
+app.use('/signup/student', _express2.default.static(folder));
+app.use('/signup/teacher', _express2.default.static(folder));
+app.use('/profile', _express2.default.static(folder));
+app.use('/group', _express2.default.static(folder));
+app.use('/group/:id', _express2.default.static(folder));
+app.use('/task', _express2.default.static(folder));
+app.use('/task/:id', _express2.default.static(folder));
+app.use('/recover', _express2.default.static(folder));
+app.use('/recover/:token', _express2.default.static(folder));
+app.use('/info', _express2.default.static(folder));
+app.use('/info/rules', _express2.default.static(folder));
+
 app.use(_bodyParser2.default.json());
 app.use('/api', _routes2.default);
+// app.use(expressJWT({ secret: secret }).unless({ path: ['api/login', '/'] }));
 
 exports.default = app;
 //# sourceMappingURL=app.js.map
